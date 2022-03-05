57c8b6824706022c473f896baee3070c61c20cac
Aragas <personal@aragas.org>
2022-03-05 17:00:05 +0300

    Added Localizer
    Minor stuff

---
+++ src/BUTR.Site.NexusMods.Client/Utils/DataGridUtils.cs
@@ -0,0 +1,24 @@
+﻿using Blazorise.DataGrid;
+
+namespace BUTR.Site.NexusMods.Client.Utils
+{
+    public static class DataGridUtils
+    {
+        public static void SelectDeselect<TEntry>(TEntry? model, ref TEntry? entry, ref DataGrid<TEntry> dataGrid) where TEntry : class
+        {
+            if (entry != model)
+            {
+                entry = model;
+            }
+            else if (entry is not null)
+            {
+                dataGrid.SelectedRow = null;
+                entry = null;
+            }
+            else
+            {
+                entry = model;
+            }
+        }
+    }
+}
---
+++ src/BUTR.Site.NexusMods.Client/Utils/LocalizationUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BUTR.Site.NexusMods.Client.Utils
+{
+    public static class LocalizationUtils
+    {
+        public static bool IsTranslationString(in ReadOnlySpan<char> span)
+        {
+            if (span.Length < 3)
+                return false;
+
+            if (span[0] != '{' || span[1] != '=')
+                return false;
+
+            if (span.IndexOf('}') == -1)
+                return false;
+
+            return true;
+        }
+
+        public static bool TryParseTranslationString(in ReadOnlySpan<char> span, out string id, out string content)
+        {
+            id = string.Empty;
+            content = string.Empty;
+
+            if (span.Length < 3)
+                return false;
+
+            if (span[0] != '{' || span[1] != '=')
+                return false;
+
+            if (span.IndexOf('}') is var idx && idx == -1)
+                return false;
+
+            id = new string(span.Slice(0, idx + 1));
+            content = new string(span.Slice(idx + 1));
+            return true;
+        }
+    }
+}
