8c9540e63d0e3c6e0a6e02100b551d0efaf982ba
Aragas <personal@aragas.org>
2022-07-14 23:52:35 +0300

    Added a cooler loading bar for client
    Filter/sort has a loading bar too

---
+++ src/BUTR.Site.NexusMods.Client/Utils/ProgressUtils.cs
@@ -0,0 +1,43 @@
+﻿using Blazorise.Utilities;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BUTR.Site.NexusMods.Client.Utils
+{
+    public static class ProgressUtils
+    {
+        public static IAsyncDisposable DoProgress(Action<int> setProgress, Func<int> getProgress, Func<Task> triggerStateChange)
+        {
+            var cts = new CancellationTokenSource();
+            setProgress(0);
+            var task = Task.Run(async () =>
+            {
+                while (cts is not null && !cts.IsCancellationRequested)
+                {
+                    if (getProgress() == 100) break;
+                    if (getProgress() > 70)
+                    {
+                        setProgress(getProgress() + 1);
+                        await triggerStateChange();
+                        await Task.Delay(60, cts.Token);
+                    }
+                    else
+                    {
+                        setProgress(getProgress() + 5);
+                        await triggerStateChange();
+                        await Task.Delay(30, cts.Token);
+                    }
+                }
+            }, cts.Token);
+            return AsyncDisposable.Create(async () =>
+            {
+                cts.Cancel();
+                await Task.WhenAny(task);;
+                task.Dispose();
+                cts.Dispose();
+            });
+        }
+    }
+}
