564df794eff8028bc88307aa096006783517e4e8
Vitalii Mikhailov <personal@aragas.org>
2023-01-26 18:59:41 +0200

    Added linked mods count

--- src/BUTR.Site.NexusMods.Server/Controllers/DiscordController.cs
+++ src/BUTR.Site.NexusMods.Server/Controllers/DiscordController.cs
@@ -1,14 +1,18 @@
 ﻿using BUTR.Authentication.NexusMods.Authentication;
+using BUTR.Site.NexusMods.Server.Contexts;
 using BUTR.Site.NexusMods.Server.Extensions;
 using BUTR.Site.NexusMods.Server.Models.API;
+using BUTR.Site.NexusMods.Server.Models.Database;
 using BUTR.Site.NexusMods.Server.Services;
 using BUTR.Site.NexusMods.Shared.Helpers;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 using System;
+using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,13 +25,16 @@ namespace BUTR.Site.NexusMods.Server.Controllers
         private sealed record Metadata(
             [property: JsonPropertyName(DiscordConstants.BUTRModAuthor)] int IsModAuthor,
             [property: JsonPropertyName(DiscordConstants.BUTRModerator)] int IsModerator,
-            [property: JsonPropertyName(DiscordConstants.BUTRAdministrator)] int IsAdministrator);
+            [property: JsonPropertyName(DiscordConstants.BUTRAdministrator)] int IsAdministrator,
+            [property: JsonPropertyName(DiscordConstants.BUTRLinkedMods)] int LinkedMods);
         
         private readonly DiscordClient _discordClient;
+        private readonly AppDbContext _dbContext;
 
-        public DiscordController(DiscordClient discordClient)
+        public DiscordController(DiscordClient discordClient, AppDbContext dbContext)
         {
             _discordClient = discordClient ?? throw new ArgumentNullException(nameof(discordClient));
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
         }
         
         [HttpGet("GetOAuthUrl")]
@@ -56,8 +63,20 @@ namespace BUTR.Site.NexusMods.Server.Controllers
         public async Task<ActionResult> UpdateMetadata([FromBody] DiscordAccessTokenModel body)
         {
             var role = HttpContext.GetRole();
+            var userId = HttpContext.GetUserId();
 
-            var result = await _discordClient.PushMetadata(body.AccessToken, new Metadata(1, role == ApplicationRoles.Moderator ? 1 : 0, role == ApplicationRoles.Administrator ? 1 : 0));
+            var linkedModsCount = await _dbContext
+                .Set<NexusModsModEntity>()
+                .CountAsync(y => y.UserIds.Contains(userId));
+            var manuallyLinkedModsCount = await _dbContext
+                .Set<UserAllowedModsEntity>()
+                .CountAsync(y => y.UserId == userId);
+            
+            var result = await _discordClient.PushMetadata(body.AccessToken, new Metadata(
+                1,
+                role == ApplicationRoles.Moderator ? 1 : 0,
+                role == ApplicationRoles.Administrator ? 1 : 0,
+                linkedModsCount + manuallyLinkedModsCount));
             return StatusCode(result ? StatusCodes.Status200OK : StatusCodes.Status400BadRequest);
         }
         
--- src/BUTR.Site.NexusMods.Shared/Helpers/DiscordRoles.cs
+++ src/BUTR.Site.NexusMods.Shared/Helpers/DiscordRoles.cs
@@ -5,5 +5,6 @@ namespace BUTR.Site.NexusMods.Shared.Helpers
         public const string BUTRModAuthor = "butrmodauthor";
         public const string BUTRModerator = "butrmoderator";
         public const string BUTRAdministrator = "butradministrator";
+        public const string BUTRLinkedMods = "butrlinkedmods";
     }
 }
