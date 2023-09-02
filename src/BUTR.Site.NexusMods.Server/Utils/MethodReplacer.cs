e7eb3907fcd63927b793d59b7ccadac9f30667ad
Vitalii Mikhailov <personal@aragas.org>
2023-09-03 01:17:51 +0300

    Fix

--- src/BUTR.Site.NexusMods.Server/Utils/MethodReplacer.cs
+++ src/BUTR.Site.NexusMods.Server/Utils/MethodReplacer.cs
@@ -49,8 +49,17 @@ public static class MethodReplacer
                     0xC3  // ret
                 }).ToArray();
 
-        VirtualProtect(sourceAddress, (uint) instruction.Length, Protection.PAGE_EXECUTE_READWRITE, out var old);
+        var oldWindows = default(Protection);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            _ = Syscall.mprotect(sourceAddress, (ulong) instruction.Length, MmapProts.PROT_WRITE | MmapProts.PROT_READ);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            _ = VirtualProtect(sourceAddress, (uint) instruction.Length, Protection.PAGE_EXECUTE_READWRITE, out oldWindows);
+
         Marshal.Copy(instruction, 0, sourceAddress, instruction.Length);
-        VirtualProtect(sourceAddress, (uint) instruction.Length, old, out _);
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            _ = Syscall.mprotect(sourceAddress, (ulong) instruction.Length, MmapProts.PROT_EXEC | MmapProts.PROT_READ);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            _ = VirtualProtect(sourceAddress, (uint) instruction.Length, oldWindows, out _);
     }
 }
