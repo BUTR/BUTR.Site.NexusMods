429a935b37c97c7affe4c431d6c673b63f368e35
Vitalii Mikhailov <personal@aragas.org>
2024-10-16 14:06:30 +0300

    Fix

--- src/BUTR.Site.NexusMods.Server/Migrations/20241016101519_SteamWorkshop2.cs
+++ src/BUTR.Site.NexusMods.Server/Migrations/20241016101519_SteamWorkshop2.cs
@@ -10,6 +10,32 @@ namespace BUTR.Site.NexusMods.Server.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropForeignKey(
+                name: "FK_steamworkshop_mod_file_update_steamworkshop_mod_tenant_stea~",
+                table: "steamworkshop_mod_file_update",
+                schema: "steamworkshop_mod");
+            
+            migrationBuilder.DropForeignKey(
+                name: "FK_steamworkshop_mod_module_steamworkshop_mod_tenant_steamwork~",
+                table: "steamworkshop_mod_module",
+                schema: "steamworkshop_mod");
+            
+            migrationBuilder.DropForeignKey(
+                name: "FK_steamworkshop_mod_name_steamworkshop_mod_tenant_steamworksh~",
+                table: "steamworkshop_mod_name",
+                schema: "steamworkshop_mod");
+            
+            migrationBuilder.DropForeignKey(
+                name: "FK_nexusmods_user_steamworkshop_mod_steamworkshop_mod_tenant_s~",
+                table: "nexusmods_user_steamworkshop_mod",
+                schema: "nexusmods_user");
+            
+            migrationBuilder.DropForeignKey(
+                name: "FK_crash_report_module_info_steamworkshop_mod_tenant_steamwork~",
+                table: "crash_report_module_info",
+                schema: "crashreport");
+            
+            
             migrationBuilder.AlterColumn<string>(
                 name: "steamworkshop_mod_name_id",
                 schema: "steamworkshop_mod",
@@ -64,6 +90,57 @@ namespace BUTR.Site.NexusMods.Server.Migrations
                 oldClrType: typeof(int),
                 oldType: "integer",
                 oldNullable: true);
+            
+            
+            migrationBuilder.AddForeignKey(
+                name: "FK_steamworkshop_mod_file_update_steamworkshop_mod_tenant_stea~",
+                schema: "steamworkshop_mod",
+                table: "steamworkshop_mod_file_update",
+                columns: ["tenant", "steamworkshop_mod_file_update_id"],
+                principalSchema: "steamworkshop_mod",
+                principalTable: "steamworkshop_mod",
+                principalColumns: ["tenant", "steamworkshop_mod_id"],
+                onDelete: ReferentialAction.Cascade);
+            
+            migrationBuilder.AddForeignKey(
+                name: "FK_steamworkshop_mod_module_steamworkshop_mod_tenant_steamwork~",
+                schema: "steamworkshop_mod",
+                table: "steamworkshop_mod_module",
+                columns: ["tenant", "steamworkshop_mod_module_id"],
+                principalSchema: "steamworkshop_mod",
+                principalTable: "steamworkshop_mod",
+                principalColumns: ["tenant", "steamworkshop_mod_id"],
+                onDelete: ReferentialAction.Cascade);
+            
+            migrationBuilder.AddForeignKey(
+                name: "FK_steamworkshop_mod_name_steamworkshop_mod_tenant_steamworksh~",
+                schema: "steamworkshop_mod",
+                table: "steamworkshop_mod_name",
+                columns: ["tenant", "steamworkshop_mod_name_id"],
+                principalSchema: "steamworkshop_mod",
+                principalTable: "steamworkshop_mod",
+                principalColumns: ["tenant", "steamworkshop_mod_id"],
+                onDelete: ReferentialAction.Cascade);
+            
+            migrationBuilder.AddForeignKey(
+                name: "FK_nexusmods_user_steamworkshop_mod_steamworkshop_mod_tenant_s~",
+                schema: "nexusmods_user",
+                table: "nexusmods_user_steamworkshop_mod",
+                columns: ["tenant", "steamworkshop_mod_id"],
+                principalSchema: "steamworkshop_mod",
+                principalTable: "steamworkshop_mod",
+                principalColumns: ["tenant", "steamworkshop_mod_id"],
+                onDelete: ReferentialAction.Cascade);
+            
+            migrationBuilder.AddForeignKey(
+                name: "FK_crash_report_module_info_steamworkshop_mod_tenant_steamwork~",
+                schema: "crashreport",
+                table: "crash_report_module_info",
+                columns: ["tenant", "steamworkshop_mod_id"],
+                principalSchema: "steamworkshop_mod",
+                principalTable: "steamworkshop_mod",
+                principalColumns: ["tenant", "steamworkshop_mod_id"],
+                onDelete: ReferentialAction.SetNull);
         }
 
         /// <inheritdoc />
